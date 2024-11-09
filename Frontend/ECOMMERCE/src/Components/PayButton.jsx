60479851ec9ec2336627ccda2e98ad21af347737
Pratham Rawat <prathamrawat2004@gmail.com>
2024-11-09 17:47:54 +0530

    changed the stripe checkout

--- Frontend/ECOMMERCE/src/Components/PayButton.jsx
+++ Frontend/ECOMMERCE/src/Components/PayButton.jsx
@@ -1,28 +1,22 @@
 import React from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 const PayButton = ({ cartItems }) => {
-  const navigate = useNavigate();
   const handleCheckout = () => {
-    axios
-      .post(
-        "https://buynest-ecommerce-backend-27.onrender.com/Stripe/create-checkout-session",
-        { cartItems }
-      )
+    axios.post("https://buynest-ecommerce-backend-27.onrender.com/Stripe/create-checkout-session", { cartItems })
       .then((res) => {
         if (res.data?.url) {
-          navigate("/checkout-success"); // Redirect to your checkout-success route
+          window.location.href = res.data.url; // Redirect to Stripe Checkout
         }
       })
       .catch((err) => console.log(err.message));
-  };
+  }
+
+  
 
   return (
-    <button className="btn btn-primary" onClick={() => handleCheckout()}>
-      Check Out
-    </button>
-  );
-};
+    <button className="btn btn-primary" onClick={() => handleCheckout()}>Check Out</button>
+  )
+}
 
 export default PayButton;
